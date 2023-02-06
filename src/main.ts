0da8cba8cc1ef8a6836968bffcb57c689fd90c1c
Vadzim Aleshchyk <folent1@yandex.ru>
2023-02-07 01:51:45 +0300

    feat: add settings for server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.enableCors({
+    allowedHeaders: '*'
+  });
+
+  app.useGlobalPipes(new ValidationPipe()); 
+
+  const config = new DocumentBuilder()
+  .setTitle('Home Library Service')
+  .setVersion('1.0.0')
+  .setDescription('Home music library service')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(parseInt(process.env.PORT, 10) || 4000);
 }
 bootstrap();
