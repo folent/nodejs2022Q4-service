0416e88bc15ab542627abaa26264b85e393282bc
Vadzim Aleshchyk <folent1@yandex.ru>
2023-02-20 15:27:50 +0300

    feat: change structure for user

---
+++ src/user/User.entity.ts
@@ -0,0 +1,54 @@
+import { Exclude } from "class-transformer";
+import { IsNotEmpty } from "class-validator";
+import { Column, CreateDateColumn, DatabaseType, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string; // uuid v4
+
+    @Column()
+    login: string;
+
+    @Column()
+    @Exclude()
+    password: string;
+
+    @Column({
+      default: 1
+    })
+    // @Generated('increment')
+    version: number; // integer number, increments on update
+
+    @Column({
+      nullable: true,
+      type: 'bigint',
+      transformer: {
+          from(value: string): number {
+              return Number(value);
+          },
+          to(value) {
+              return value;
+          }
+        }
+    })
+    createdAt: number; // timestamp of creation
+
+    @Column({
+      nullable: true,
+      type: 'bigint',
+      transformer: {
+          from(value: string): number {
+              return Number(value);
+          },
+          to(value) {
+              return value;
+          }
+        }
+    })
+    updatedAt: number; // timestamp of last update
+
+    constructor(partial: Partial<User>) {
+        Object.assign(this, partial);
+      }
+  }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,108 @@
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, ParseUUIDPipe, Post, Put, Req, Res, UseInterceptors } from '@nestjs/common';
+import { isUUID } from 'class-validator';
+import { User } from 'src/user/User.entity';
+import { UserService } from './user.service';
+import { v4 } from 'uuid'
+import { Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ApiBadRequestResponse, ApiForbiddenResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './CreateUser.dto';
+import { UpdatePasswordDto } from './UpdatePassword.dto';
+
+@ApiTags('user')
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get all users' })
+  async getUsers() {
+    const users = await this.userService.getUsers();
+    return users
+  }
+  @Get('/:id')
+  @ApiOperation({ summary: 'Get user by id' })
+  @ApiBadRequestResponse({ description: 'Validation error' })
+  @ApiNotFoundResponse({ description: 'User is not found' })
+  @UseInterceptors(ClassSerializerInterceptor)
+  async getUser(@Param('id', ParseUUIDPipe) id: string): Promise<CreateUserDto> {
+    if (!isUUID(id)) {
+      throw new HttpException('id is not validate', HttpStatus.BAD_REQUEST)
+    }
+    const user = await this.userService.getUser(id);
+
+    if (!user) {
+      throw new HttpException('user doesn`t exists', HttpStatus.NOT_FOUND)
+    }
+    return user
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Create user' })
+  @ApiBadRequestResponse({ description: 'Validation error' })
+  @UseInterceptors(ClassSerializerInterceptor)
+  async addUser(@Req() req: Request) {
+    const newUser = new User({
+      ...req.body,
+      id: v4(),
+      createdAt: new Date().valueOf(),
+      updatedAt: new Date().valueOf()
+    })
+    const user = await this.userService.addUser(newUser);
+    
+
+    return user
+  }
+
+  @Put('/:id')
+  @ApiOperation({ summary: 'Update user' })
+  @ApiBadRequestResponse({ description: 'Validation error' })
+  @ApiNotFoundResponse({ description: 'User is not found' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @UseInterceptors(ClassSerializerInterceptor)
+  async updateUser(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() body: UpdatePasswordDto
+    ): Promise<CreateUserDto> {
+      
+    if (!isUUID(id)) {
+      throw new HttpException('id is not validate', HttpStatus.BAD_REQUEST)
+    }
+    const user = await this.userService.getUser(id);
+
+    if (!user) {
+      throw new HttpException('user doesn`t exists', HttpStatus.NOT_FOUND)
+    } 
+
+    if (user.password !== body.oldPassword) {
+      throw new HttpException('old password doesn`t correct', HttpStatus.FORBIDDEN)
+    }
+
+    const updatedUser = await this.userService.updateUser(id, body)
+
+    return updatedUser
+  }
+
+  @Delete('/:id')
+  @ApiOperation({ summary: 'Delete user' })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiNoContentResponse({ description: 'User deleted' })
+  @ApiBadRequestResponse({ description: 'Validation errors' })
+  @ApiNotFoundResponse({ description: 'User is not found' })
+  async deleteUser(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Res() res: Response
+  ){
+    if (!isUUID(id)) {
+      throw new HttpException('id is not validate', HttpStatus.BAD_REQUEST)
+    }
+    const user = await this.userService.getUser(id);
+
+    if (!user) {
+      throw new HttpException('user doesn`t exists', HttpStatus.NOT_FOUND)
+    }
+    await this.userService.deleteUser(id);
+
+    return res.status(StatusCodes.NO_CONTENT).json(id)
+  }
+}
